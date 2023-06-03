8c6772c0106e54f36120fa28984c24f3eb945bd8
Vivek Yadav <vy151617@gmail.com>
2023-06-03 22:50:37 +0530

    All the basic steup done

--- index.js
+++ index.js
@@ -1,62 +1,95 @@
 const express = require('express');
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const connectToMongo = require('./db');
+const { campgroundSchema } = require('./joiSchemas');
+const ExpressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 connectToMongo();
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // to parse the req.body
 app.use(methodOverride('_method'));
 
-app.get('/', async (req, res) => {
-    res.render('home');
-})
+//-----------------Joi Validation
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        // Since details is array of object
+        const msg = error.details.map(el => el.message).join(', ')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
 
-// Get all campgrounds
-app.get('/campgrounds', async (req, res) => {
+//-------------------Configuring static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//-------------------Home page route
+app.get('/', catchAsync(async (req, res) => {
+    res.render('home');
+}))
+
+//-------------------Get all campgrounds
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-})
+}))
 
-// Create a new campground
+//-------------------Create a new campground
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
-app.post('/campgrounds', async (req, res) => {
+
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-// Get info about a single campground
-app.get('/campgrounds/:id', async (req, res) => {
+//-------------------Get info about a single campground
+app.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-})
+}))
 
-// Edit a campground info
-app.get('/campgrounds/:id/edit', async (req, res) => {
+//-------------------Edit a campground info
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-})
-app.put('/campgrounds/:id', async (req, res) => {
+}))
+
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}))
 
-// Delete a campground
-app.delete('/campgrounds/:id', async (req, res) => {
+//-------------------Delete a campground
+app.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+//-------------------Handelling errors
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 })
 
+app.use((err, req, res, next) => {
+    const { stausCode = 500, message = 'Something went wrong' } = err;
+    res.status(stausCode).render('error', { err });
+})
 
 app.listen(3000, () => {
     console.log('On port 3000');
--- seeds/index.js
+++ seeds/index.js
@@ -6,14 +6,31 @@ const Campground = require('../models/campground');
 
 connectToMongo();
 const sample = array => array[Math.floor(Math.random() * array.length)];
+const images = [
+    'https://res.cloudinary.com/dhbiouaym/image/upload/v1632636239/YelpCamp/mt7vew46iuelyjperm02.jpg',
+    'https://res.cloudinary.com/dhbiouaym/image/upload/v1632636244/YelpCamp/trvehxjujs8frbrjrfil.jpg',
+    'https://res.cloudinary.com/dhbiouaym/image/upload/v1632636247/YelpCamp/amniou07rihgxtsagsca.jpg',
+    'https://res.cloudinary.com/dhbiouaym/image/upload/v1632636248/YelpCamp/t5xqmk2ijt0o0shdbkkh.jpg',
+    'https://res.cloudinary.com/dhbiouaym/image/upload/v1632636469/YelpCamp/qry2l5ntcxmpstungx9q.jpg',
+    'https://res.cloudinary.com/dhbiouaym/image/upload/v1632636471/YelpCamp/kan7ktxqgynjudblesv8.jpg',
+    'https://res.cloudinary.com/dhbiouaym/image/upload/v1632636474/YelpCamp/y5xf7zdzgn5vi444qpsx.jpg',
+    'https://res.cloudinary.com/dhbiouaym/image/upload/v1632636476/YelpCamp/qmmxwz5prukysbohozaz.jpg'
+
+]
 
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const random8 = Math.floor(Math.random() * 8);
+        const price = Math.floor(Math.random() * 20) + 10;
+
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: images[random8],
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias modi libero exercitationem excepturi nobis recusandae assumenda consequatur fugit omnis, nihil voluptates dolore, provident sequi minus sint iusto. Esse, asperiores velit.',
+            price: price
         })
         await camp.save();
     }
