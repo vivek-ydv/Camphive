e205b87f301ca1168b3f0c1782ce6242754b8c2a
Vivek Yadav <vy151617@gmail.com>
2023-07-06 23:13:20 +0530

    Minor fixes

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/camphiveApp";
+const mongoURI = process.env.mongoURI;
 
 const connectToMongo = () => {
     mongoose.set('strictQuery', false);
